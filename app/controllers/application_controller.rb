710b9c2cf21a0e4048ad6f2ec42da6eb95733b36
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-20 01:13:28 +0300

    [Backend:Add] Create article,article_comments,article_comment_replies resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
-    def index
-        puts("ADmin is now created")
-        puts("Test successful")
-    end
+private
+
+#Handle record missing
+def render_not_found(exception)
+    render json:{errors:exception}, status: :not_found
+end
+
+#Handle invalid
+def render_invalid(exception)
+    render json:{errors:exception.record.errors.full_messages}
+end
+
+    
 end
