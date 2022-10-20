c8d7791d3ad44586cade2399c469c736f308d638
Kelvin <Kariuki405@gmail.com>
2022-10-20 12:33:46 +0300

    [Client-Side] Added material UI and README

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Routes,Route } from 'react-router-dom';
 import Login from './components/Login/Login';
+import TopBar from './components/topbar/TopBar';
 
 export default function App() {
   return (
     <div>
-      <h1>Motivation app</h1>
+      {/* <h1>Motivation app</h1>
       <Routes>
         <Route path="/" element={<Login />} />
-      </Routes>
+      </Routes> */}
+    <TopBar/>
     </div>
   )
 }
