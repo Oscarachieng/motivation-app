e5cc71e845c3d5e73cd8940832183b7c33918ebb
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-20 09:54:07 +0300

    [Back-end] Creates the show action for the staff controller and shifts error handling definition to Application controller

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,5 +1,5 @@
 class StaffsController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound,with: :render_record_not_found
+# rescue_from ActiveRecord::RecordNotFound,with: :render_record_not_found
 
 
     wrap_parameters false
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :staffs, only: [:index, :show]
-  resources :admins, only: [:index]
+  resources :admins, only: [:index, :show]
   
 end
