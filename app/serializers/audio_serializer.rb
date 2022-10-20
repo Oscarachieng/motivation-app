6b38b6c4fd4cc3fe0c437a64d1bb2cf2a7e51a47
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-20 04:08:45 +0300

    [BACKEND:Add] Created audio_comment model resource

--- app/serializers/audio_serializer.rb
+++ app/serializers/audio_serializer.rb
@@ -1,3 +1,4 @@
 class AudioSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :is_approved, :likes, :is_flagged
+  has_many :audio_comments
 end
---
+++ db/migrate/20221020004923_create_audio_comments.rb
@@ -0,0 +1,11 @@
+class CreateAudioComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :audio_comments do |t|
+      t.string :comment
+      t.integer :likes
+      t.belongs_to :audio, null:false, foreign_key: true
+      t.belongs_t :user,null:false, foreign_key:true
+      t.timestamps
+    end
+  end
+end
