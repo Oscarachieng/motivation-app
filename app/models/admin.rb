561a0d828196d2115e9f92a84a04c7efb4544b10
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-21 15:53:55 +0300

    Adds adamin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,13 @@
 class Admin < ApplicationRecord
+    has_secure_password
     #validations for the admin model
-    validates_presence_of :first_name, :last_name, :password
-    validates :password, length: { minimu:12, maximum: 24}
+    validates_presence_of :first_name,  :last_name, :password, :email
+    
+    validates :email, uniqueness: true
+    validates :password, length: { in: 12..24 } 
    
+    #GET index 
    
+    
    
 end
