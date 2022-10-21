aee35c510e0bc5d011a12ed67726605c9a88757a
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-10-21 12:00:49 +0300

    Update subscriptions_controller.rb

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,5 @@
 class SubscriptionsController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+   
 
     # GET /subscriptions
     def index
@@ -45,11 +44,5 @@ class SubscriptionsController < ApplicationController
       params.permit(:student_id, :category_id)
     end
   
-    def render_not_found_response
-      render json: { error: "Subscription not found" }, status: :not_found
-    end
-
-    def render_unprocessable_entity_response(invalid)
-        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
-    end
+   
 end
