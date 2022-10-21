439dac4fd1121c1ea3e86040115779811b8b1985
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-21 14:24:50 +0300

    [BACKEND:Fix]Fixing the conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :article_comment_replies
   resources :article_comments
   resources :articles
-  resources :staffs
   resources :admins, only: [:index]
   
 end
