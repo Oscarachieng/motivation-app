7f847ef13894721ff44767a08c316bf7d1ce7636
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-21 13:55:00 +0300

    [BACKEND:Add]Adding audio routes:

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :video_comment_replies
   resources :video_comments
   resources :videos
+  resources :audio_comment_replies
+  resources :audio_comments
+  resources :audios
   resources :article_comment_replies
   resources :article_comments
   resources :articles
