0ccff30a723114831b2e8d73e91291890ce7131a
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-10-21 14:42:46 +0300

    Update 20221019214407_create_article_comment_replies.rb

--- db/migrate/20221019214407_create_article_comment_replies.rb
+++ db/migrate/20221019214407_create_article_comment_replies.rb
@@ -1,11 +1,5 @@
 class CreateArticleCommentReplies < ActiveRecord::Migration[7.0]
   def change
-    create_table :article_comment_replies do |t|
-      t.integer :likes
-      t.string :reply
-      # t.belongs_to :user, null:false, foreign_key:true
-      t.belongs_to :article_comment,null:false, foreign_key:true
-      t.timestamps
-    end
+     
   end
 end
