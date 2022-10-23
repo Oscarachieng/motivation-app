1a107b15a3074899cd14b61c87459d0253f08362
Kelvin <Kariuki405@gmail.com>
2022-10-24 00:30:53 +0300

    [Client-Side]Adding a Function for styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,25 @@ import { Routes,Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import TopBar from './components/topbar/TopBar';
 import Sidebar from './components/sidebar/Sidebar';
+import { red } from '@material-ui/core/colors';
 
 export default function App() {
+  const mystyles ={
+    background:"#001E2A",
+    textAlign:"center",
+    padding: 10,
+    textTransform:"uppercase",
+    color:"#FF8C00",
+    marginBottom: 30,
+  }
   return (
     <div>
-      {/* <h1>Motivation app</h1> */}
-      {/* <Routes>
+      <h1 style={mystyles}>Motivation app</h1> 
+       <Routes>
         <Route path="/" element={<Login />} />
-      </Routes> */}
-    <TopBar/>
-    <Sidebar />
+      </Routes>
+    {/* <TopBar/>
+    <Sidebar /> */}
     </div>
   )
 }
