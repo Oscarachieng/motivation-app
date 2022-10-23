6344f64f7654ac23e3eb9beb3fd2592a9e44b599
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-10-23 16:25:33 +0300

    Delete admin.rb

--- app/models/admin.rb
+++
@@ -1,8 +0,0 @@
-class Admin < ApplicationRecord
-    has_secure_password
-    #validations for the admin model
-    validates_presence_of :first_name,  :last_name, :password, :email    
-    validates :email, uniqueness: true
-    validates :password, length: { in: 12..24 }    
-   
-end
