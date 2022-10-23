1f831adf095058fd053acb9dbee0ce3dbffae16d
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-23 16:58:16 +0300

    Adds the validations for the admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,8 @@ class Admin < ApplicationRecord
 
     validates_presence_of :first_name, :last_name, :email, :password 
 
+    validates :email, uniqueness: true
+
+    validates :password, length: { in: 12..24 }
+
 end
