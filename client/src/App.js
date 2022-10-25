3b2ae94edfa6cbf4cc01a9c1510e32a998c37817
Kelvin <Kariuki405@gmail.com>
2022-10-26 02:20:21 +0300

    [Client-side]Routing app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
+// import { Routes,Route } from 'react-router-dom';
 // import TopBar from './components/topbar/TopBar';
 // import Sidebar from './components/sidebar/Sidebar';
 
-import Login from './components/Login/Login';
-import Profile from './components/Profile/Profile';
-import TopBar from './components/topbar/TopBar';
+// import Login from './components/Login/Login';
+// import Profile from './components/Profile/Profile';
+// import TopBar from './components/topbar/TopBar';
 
 
 export default function App() {
-  const mystyles ={
-    background:"#001E2A",
-    textAlign:"center",
-    padding: 10,
-    textTransform:"uppercase",
-    color:"#FF8C00",
-    marginBottom: 30,
+  // const mystyles ={
+  //   background:"#001E2A",
+  //   textAlign:"center",
+  //   padding: 10,
+  //   textTransform:"uppercase",
+  //   color:"#FF8C00",
+  //   marginBottom: 30,
   }
-  return (
-    <div>
-
-    </div>
-  )
-}
+return 
 
