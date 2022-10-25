c0428243a20c4645a9e67abcf41c31d339724e19
Newton <newtieblackie@gmail>
2022-10-25 08:28:43 +0300

    [Frontend] homepage demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-// import { Routes,Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 // import TopBar from './components/topbar/TopBar';
 // import Sidebar from './components/sidebar/Sidebar';
 
 import Login from './components/Login/Login';
+import Home from './page/home/Home';
+import Product from './page/product/Product';
+import UserList from './page/userList/UserList';
+import User from './page/user/User';
+import NewUser from './page/newUser/NewUser';
+import ProductList from './page/productList/ProductList';
+import NewProduct from './page/newProduct/NewProduct';
 // import Profile from './components/Profile/Profile';
 // import TopBar from './components/topbar/TopBar';
 
@@ -18,9 +25,20 @@ export default function App() {
   //   marginBottom: 30,
   // }
   return (
-    <div>
-<Login />
+    
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/user/:userId" element={<User />} />
+        <Route path="/newUser" element={<NewUser />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/newProduct" element={<NewProduct />} />
+      </Routes>
+      <Home />
+      <Login />
     </div>
-  )
+  );
 }
 
