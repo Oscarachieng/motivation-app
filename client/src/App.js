34c6a7e24ef157f9b215a52970e5395f0a6cb713
Newton <newtieblackie@gmail>
2022-10-26 14:00:35 +0300

    [Frontend]

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import { Routes,Route } from 'react-router-dom';
-import TopBar from './components/topbar/TopBar';
-import Sidebar from './components/sidebar/Sidebar';
-import Student from './components/StudentFolder/Student';
-import Staff from './components/Staffpage/Staff';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import TopBar from "./components/topbar/TopBar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Student from "./components/StudentFolder/Student";
+import Staff from "./components/Staffpage/Staff";
 
-// import Login from './components/Login/Login';
-import Home from './components/home/Home';
-// import Product from './page/product/Product';
-// import UserList from './page/userList/UserList';
-// import User from './page/user/User';
-// import NewUser from './page/newUser/NewUser';
-// import ProductList from './page/productList/ProductList';
-// import NewProduct from './page/newProduct/NewProduct';
-// import Profile from './components/Profile/Profile';
-// import TopBar from './components/topbar/TopBar';
+import Login from "./components/Login/Login";
+import Home from "./components/home/Home";
+import Product from "./page/product/Product";
+import UserList from "./page/userList/UserList";
+import User from "./page/user/User";
+import NewUser from "./page/newUser/NewUser";
+import ProductList from "./page/productList/ProductList";
+import NewProduct from "./page/newProduct/NewProduct";
+import Profile from "./components/Profile/Profile";
 
 
 export default function App() {
-
   return (
-    
     <div className="">
-      {/* <TopBar /> */}
+      <TopBar />
       <Routes>
-      <Route path="/" element={<Staff />} />
-      <Route path="/" element={<Student />} />
-      
-        <Route path="/" element={<Sidebar />} />
-        {/* <Route path="/users" element={<UserList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/users" element={<UserList />} />
         <Route path="/user/:userId" element={<User />} />
         <Route path="/newUser" element={<NewUser />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/product/:productId" element={<Product />} />
-        <Route path="/newProduct" element={<NewProduct />} /> */}
+        <Route path="/newProduct" element={<NewProduct />} />
       </Routes>
-     
     </div>
   );
 }
-
